030288e7cb69ec865133b7eed25553a676afa619
Max Lehmann <mightmax@gmx.de>
2021-01-12 16:57:58 +0100

    Button adaptet , some cosmetic changes

---
+++ app/src/main/java/com/example/finanzapp/ui/logout/LogoutActivity.java
@@ -0,0 +1,16 @@
+package com.example.finanzapp.ui.logout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.finanzapp.R;
+
+public class LogoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+    }
+}
